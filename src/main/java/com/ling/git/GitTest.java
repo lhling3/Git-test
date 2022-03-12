7b1d6eb6d5d66d120bca9b7ef79b5edce73af3db
Ling <Ling@163.com>
2022-03-12 20:12:10 +0800

    second commit

--- src/main/java/com/ling/git/GitTest.java
+++ src/main/java/com/ling/git/GitTest.java
@@ -3,5 +3,7 @@ package com.ling.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello 111");
+        System.out.println("hello 222");
     }
 }
