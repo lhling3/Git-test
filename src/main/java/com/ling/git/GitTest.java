a11a55f9dddc66377a7691d77bcf21184137e487
lhling3 <72916584+lhling3@users.noreply.github.com>
2022-03-13 10:14:15 +0800

    Update GitTest.java

--- src/main/java/com/ling/git/GitTest.java
+++ src/main/java/com/ling/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello 444");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("from git-hub");
     }
 }
